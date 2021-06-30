fa62b00e345e972ece549efc38f4597762e40d84
Ram Prasad Reddy. Maddi <ram.prasad@introhive.com>
2021-06-30 14:08:06 +0530

    Questions related database table, model and controller.

---
+++ db/migrate/20210629081040_create_questions.rb
@@ -0,0 +1,11 @@
+class CreateQuestions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :questions do |t|
+      t.text :question
+      t.text :description
+      t.string :tag
+
+      t.timestamps
+    end
+  end
+end
